516945e0d495e6dafe200b196a2f9dc35ff1842d
Moony <moonylight247@gmail.com>
2020-07-25 03:58:04 +0900

    캐릭터 움직임

---
+++ Assets/Scripts/Moony/MoveController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float jumpPower = 20.0f;
+    public float gravity = 20.0f;
+    private bool JumpKey = false;
+    private Vector3 Direction;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                JumpKey = true;
+            }
+
+        }
+
+
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            Direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            Direction = transform.TransformDirection(Direction);
+            Direction *= speed;
+
+        }
+        if (JumpKey)
+        {
+            Direction.y = jumpPower;
+            JumpKey = false;
+        }
+
+
+        Direction.y -= gravity * Time.deltaTime;
+        controller.Move(Direction * Time.deltaTime);
+    }
+}
