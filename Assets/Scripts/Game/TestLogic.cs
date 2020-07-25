9e786be210b10f68543df1d9958f5d50cf6c5e0f
DaeHyun Kang <limitedcopy@gmail.com>
2020-07-25 11:52:58 +0900

    M

--- Assets/Scripts/Game/TestLogic.cs
+++ Assets/Scripts/Game/TestLogic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TestLogic : MonoBehaviour
 {
     public GameObject box;
+    public SpringObject spring;
     public bool isGrab = false;
     // Start is called before the first frame update
     void Start()
