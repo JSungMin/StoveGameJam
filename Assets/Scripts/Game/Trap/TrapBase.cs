63fb70ea8767e664750e32e48e3a7810d97635ad
DaeHyun Kang <limitedcopy@gmail.com>
2020-07-25 12:41:45 +0900

    TrapBase Added

---
+++ Assets/Scripts/Game/Trap/TrapBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface ITrapReceiver
+{
+    void OnTrapped(TrapBase trap);
+}
+
+public abstract class TrapBase : MonoBehaviour
+{
+    public Collider trapTrigger;
+    public List<Collider> victimCaches = new List<Collider>();
+
+    [Header("Trap In Event")]
+    public UnityEvent onTrapIn;
+    [Header("Trap Out Event")]
+    public UnityEvent onTrapOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        var cached = victimCaches.Find(x => x == other);
+        if(null != cached) return;
+        onTrapIn?.Invoke();
+        OnTrapIn(other);
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        victimCaches.Remove(other);
+        onTrapOut?.Invoke();
+        OnTrapOut(other);
+    }
+    protected abstract void OnTrapIn(Collider other);
+    protected abstract void OnTrapOut(Collider other);
+}
