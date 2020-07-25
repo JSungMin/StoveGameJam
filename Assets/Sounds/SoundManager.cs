5fbb4566de7a92d9b5c338da74c49e3bbd6be6d2
Moony <moonylight247@gmail.com>
2020-07-25 21:24:08 +0900

    AddSound

---
+++ Assets/Sounds/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip HammerSound;
+    public AudioClip StepSound;
+    public AudioClip Spring;
+    public AudioClip DoorSound;
+    public static SoundManager instance;
+    private void Awake()
+    {
+        if(SoundManager.instance==null)
+        {
+            SoundManager.instance = this;
+        }
+    }
+
+    public void PlayHammerSound()
+    {
+        audioSource.PlayOneShot(HammerSound);
+    }
+    public void PlayStepSound()
+    {
+        audioSource.PlayOneShot(StepSound);
+    }
+    public void PlaySpringSound()
+    {
+        audioSource.PlayOneShot(Spring);
+    }
+    public void PlayDoorSound()
+    {
+        audioSource.PlayOneShot(DoorSound);
+    }
+
+
+}
