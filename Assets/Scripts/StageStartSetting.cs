0b550f560956a218d75dd1b0efa01e8a18bb7ef5
d.dlzndi <66778805+d-dlzndi@users.noreply.github.com>
2020-07-26 01:03:09 +0900

    first scene

---
+++ Assets/Scripts/StageStartSetting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageStartSetting : MonoBehaviour
+{
+    public static StageStartSetting instance;
+
+    [Range(0, 20)]
+    public int firstInkCount;
+    public Transform playerFirstPosition;
+    public GameObject goalDoor;
+    public string nextStage;
+
+    public GameObject player;
+    public ItemSystem itemSys;
+
+    private void Awake()
+    {
+        instance = this;
+        player = GameObject.Find("Player");
+        player.SetActive(false);
+        itemSys = GameObject.Find("ItemUI").GetComponent<ItemSystem>();
+        goalDoor.AddComponent<StageStartSetting_GoalDoor>();
+    }
+
+    private void Start()
+    {
+        PlayerReset();
+    }
+
+    public void PlayerReset()
+    {
+        player.gameObject.transform.position = playerFirstPosition.position;
+        player.SetActive(true);
+        itemSys.ItemReset();
+        itemSys.ItemGet(Item.ink, firstInkCount);
+    }
+
+    public void NextStage()
+    {
+        DefaultUI.instance.OpenScene(nextStage);
+    }
+}
---
+++ Assets/Scripts/StageStartSetting_GoalDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageStartSetting_GoalDoor : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StageStartSetting.instance.NextStage();
+        }
+    }
+}
