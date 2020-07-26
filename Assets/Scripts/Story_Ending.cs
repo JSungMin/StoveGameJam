cf7ff7123c7d7daf94b869ff003e0aaa71ca6d97
JSungMin <jsm1505104@gmail.com>
2020-07-26 09:47:07 +0900

    video & Icon Added

--- Assets/Scripts/Story_Ending.cs
+++ Assets/Scripts/Story_Ending.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Story_Ending : MonoBehaviour
 {
-
+    public VideoPlayer video;
     void Start()
     {
         // DefaultUI.instance.OpenScene("Credit");
-        SceneManager.LoadScene("Credit");
+        //SceneManager.LoadScene("Credit");
+    }
+
+    void Update()
+    {
+        if (!video.isPlaying)
+        {
+            SceneManager.LoadScene("Credit");
+        }
     }
 
 }
